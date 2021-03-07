d7d234af8ba8fb0faff57dbbe0b476327dc5df21
dukeNashor <sonicisme@163.com>
2021-03-07 20:09:58 +0800

    halfedge cleanup by rebuilding the mesh structure.

--- CGAL_mesh_simplifier/CGAL_mesh_simplifier/main.cpp
+++ CGAL_mesh_simplifier/CGAL_mesh_simplifier/main.cpp
@@ -1,12 +1,17 @@
+ï»¿#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/polygon_mesh_processing.h>
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <chrono>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Surface_mesh.h>
-#include <CGAL/boost/graph/Euler_operations.h>
+#include<cstdlib>
 
+#include<ctime>
+
+clock_t start, end;
 using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
 
 using Point_3 = Kernel::Point_3;
@@ -18,25 +23,6 @@ using Mesh = CGAL::Surface_mesh<Point_3>;
 using sm_vertex_descriptor = boost::graph_traits<Mesh>::vertex_descriptor;
 using sm_halfedge_descriptor = boost::graph_traits<Mesh>::halfedge_descriptor;
 using sm_face_descriptor = boost::graph_traits<Mesh>::face_descriptor;
-
-using Clock = std::chrono::steady_clock;
-using std::chrono::time_point;
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
-using namespace std::literals::chrono_literals;
-
-std::vector<int> ReadIndices(const char* file_name)
-{
-    std::vector<int> indices;
-    std::ifstream in_indices(file_name, std::ios::in);
-    std::string s;
-    while (std::getline(in_indices, s)) // ÖğĞĞ¶ÁÈ¡Êı¾İ²¢´æÓÚsÖĞ£¬Ö±ÖÁÊı¾İÈ«²¿¶ÁÈ¡
-    {
-        indices.push_back(std::atoi(s.c_str()));
-    }
-
-    return indices;
-}
 
 bool DeleteFacesByVertexIndices(Mesh& sm, const std::vector<int>& idx)
 {
@@ -55,7 +41,7 @@ bool DeleteFacesByVertexIndices(Mesh& sm, const std::vector<int>& idx)
         // get vertex_descriptor;
         //auto vd = *(sm.vertices().first + i);
         auto vd = sm_vertex_descriptor(i);
-        std::cout << vd << std::endl;
+        //std::cout << vd << std::endl;
         if (vd == sm.null_vertex())
             continue;
         // get halfedge_descriptor;
@@ -87,33 +73,118 @@ bool DeleteFacesByVertexIndices(Mesh& sm, const std::vector<int>& idx)
     return true;
 }
 
+bool IsStandardHole(sm_halfedge_descriptor h, Mesh & mesh, double max_hole_diam, int min_num_hole_edges)
+{
+    int num_hole_edges = 0;
+    CGAL::Bbox_3 hole_bbox;
+    for (sm_halfedge_descriptor hc : CGAL::halfedges_around_face(h, mesh))
+    {
+        if (hc == mesh.null_halfedge())
+            continue;
+        const auto& p = mesh.point(target(hc, mesh));
+        hole_bbox += p.bbox();
+        ++num_hole_edges;
+        if (hole_bbox.xmax() - hole_bbox.xmin() > max_hole_diam) return false;
+        if (hole_bbox.ymax() - hole_bbox.ymin() > max_hole_diam) return false;
+        if (hole_bbox.zmax() - hole_bbox.zmin() > max_hole_diam) return false;
+    }
+    if (num_hole_edges < min_num_hole_edges) return false;
+    return true;
+}
+
+Mesh CleanupMesh(const Mesh& in)
+{
+    Mesh out;
+    
+    for (auto it = in.vertices_begin(); it != in.vertices_end(); ++it)
+    {
+        out.add_vertex(in.point(*it));
+    }
+
+    for (auto it = in.faces_begin(); it != in.faces_end(); ++it)
+    {
+        auto vrange = in.vertices_around_face(in.halfedge(*it));
+        auto vbegin = vrange.begin();
+        auto vend = vrange.end();
+
+        auto vd0 = *vbegin++;
+        auto vd1 = *vbegin++;
+        auto vd2 = *vbegin;
+        // assertion for 3 vertices for 1 face..
+        assert(vbegin == vend);
+        out.add_face(vd0, vd1, vd2);
+    }
+
+    return out;
+}
+
+void ExtractCycles(Mesh mesh)
+{
+    mesh.collect_garbage();
+
+    std::vector<sm_halfedge_descriptor> border_cycles;
+    CGAL::Polygon_mesh_processing::extract_boundary_cycles(mesh, std::back_inserter(border_cycles));
+    std::ofstream out("points.txt");
+    for (sm_halfedge_descriptor h : border_cycles)
+    {
+        // å­˜å‚¨åœ†å†…çš„ç‚¹
+        std::vector<Point_3> cycle;
+        for (sm_halfedge_descriptor hc : CGAL::halfedges_around_face(h, mesh))
+        {
+            if (hc == mesh.null_halfedge())
+                continue;
+            auto vd = source(hc, mesh);
+            if (vd == mesh.null_vertex())
+                continue;
+            auto pt = mesh.point(vd);
+            cycle.push_back(pt);
+            out << pt.x() << " " << pt.y() << " " << pt.z() << std::endl;
+        }
+    }
+    out.close();
+}
+
+void ExtractCyclesBAK(Mesh mesh)
+{
+    std::vector<sm_halfedge_descriptor> border_cycles;
+    CGAL::Polygon_mesh_processing::extract_boundary_cycles(mesh, std::back_inserter(border_cycles));
+    std::ofstream out("points.txt");
+    for (sm_halfedge_descriptor h : border_cycles)
+    {
+        // å­˜å‚¨åœ†å†…çš„ç‚¹
+        std::vector<Point_3> cycle;
+        for (sm_halfedge_descriptor hc : CGAL::halfedges_around_face(h, mesh))
+        {
+            if (hc == mesh.null_halfedge())
+                continue;
+            auto vd = sm_vertex_descriptor(source(hc, mesh));
+            if (vd == mesh.null_vertex())
+                continue;
+            cycle.push_back(mesh.point(source(hc, mesh)));
+            out << mesh.point(source(hc, mesh)).x() << " " << mesh.point(source(hc, mesh)).y() << " " << mesh.point(source(hc, mesh)).z() << std::endl;
+        }
+    }
+    out.close();
+}
+
 
 int main(int argc, char **argv)
 {
-    const char* indices_file = "data/indices.txt";
-    const char* mesh_file = "data/mesh_test.off";
-
-    auto idx = ReadIndices(indices_file);
 
     Mesh sm;
     std::filebuf fb;
 
-    time_point<Clock> start = Clock::now();
-    if (!fb.open (mesh_file,std::ios::in))
+    if (!fb.open("data/mesh_test.off", std::ios::in))
     {
         return EXIT_FAILURE;
     }
-    
+
     std::istream is(&fb);
 
     if (!CGAL::read_off(is, sm))
     {
         return EXIT_FAILURE;
     }
-
-    time_point<Clock> end = Clock::now();
-    milliseconds diff = duration_cast<milliseconds>(end - start);
-    std::cout << "Read: " << diff.count() << "ms" << std::endl;
 
     auto printHelper = [&sm](const std::string &str) {
         std::cout << str << " -          number_of_vertices: " << sm.number_of_vertices() << std::endl;
@@ -128,19 +199,31 @@ int main(int argc, char **argv)
 
     printHelper("before remove face");
 
-    //// for demo purpose
-    //std::vector<int> idx(100000);
-    //std::generate(idx.begin(), idx.end(), [](){ static int i = 0; return i++; });
+    std::vector<int> indices;
+    std::ifstream in_indices("data/indices.txt", std::ios::in);
+    std::string s;
+    while (std::getline(in_indices, s)) // é€è¡Œè¯»å–æ•°æ®å¹¶å­˜äºsä¸­ï¼Œç›´è‡³æ•°æ®å…¨éƒ¨è¯»å–
+    {
+        indices.push_back(std::atoi(s.c_str()));
+    }
 
-    start = Clock::now();
+    // for demo purpose
 
-    DeleteFacesByVertexIndices(sm, idx);
-
-    end = Clock::now();
-    diff = duration_cast<milliseconds>(end - start);
-    std::cout << "Process: " << diff.count() << "ms" << std::endl;
-
+    start = clock();
+    DeleteFacesByVertexIndices(sm, indices);
     printHelper("after remove face");
+
+    sm = CleanupMesh(sm);
+    printHelper("after rebuild");
+
+
+    ExtractCycles(sm);
+    end = clock();
+    double endtime = (double)(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Total time:" << endtime * 1000 << "ms" << std::endl;	//msä¸ºå•ä½
+
+
+    
 
     std::ofstream of("simplified.off");
     if (!of.good())
@@ -148,7 +231,6 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
     CGAL::write_off(of, sm);
-
-    getchar();
+    std::cout << "end!" << std::endl;
     return EXIT_SUCCESS;
 }
